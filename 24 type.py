12842a8e876a8459d7a544f01943fe89f7d4ef5f
Venya <veny.2006@mail.ru>
2023-04-07 16:25:23 +0300

    gnklvsdlksdvnklsdvnkldvnkl

--- 24 type.py
+++ 24 type.py
@@ -7,20 +7,69 @@
 # # Работа с файлом
 # f.close()
 
-# coun = 1
-# maximka = 0
-# s = open("zadanie24_1.txt").readline()      # Чтение файла в строку
+# nechet = "13579"
+# cocojumbo = []
+# bebra = ""
+# s = open("24-1.txt").readline()      # Чтение файла в строку
 # for i in range(len(s)):
-#     if s[i] == "A" and s[i+1] == "A":
-#         coun += 1
+#     if s[i] in nechet:
+#         bebra = bebra + s[i]
 #     else:
-#         maximka = max(maximka, coun)
-#         coun = 1
-# print(maximka)
+#         cocojumbo.append(bebra)
+#         bebra = ""
+# print(max(cocojumbo, key=len), cocojumbo)
 
-s = open("zadanie24_1.txt").readline()
-s = s.replace("B", " ").replace("C", " ")
-s = s.split()
+# s = open("zadanie24_1.txt").readline()
+# s = s.replace("B", " ").replace("C", " ")
+# s = s.split()
 # print(s)
 # print(max(s))        # Max к списку из строк - аккуратно!
 # print(max("AAAAA", "BBB", "C", key=len))         ## вывод по длине
+
+# coun = 1
+# last_coun = 0
+# s = open("24_3.txt").readline()
+# s = s.replace("C", " ").replace("D", " ")
+# s = s.split()
+# print(max(s, key=len),len((max(s, key=len))))
+
+
+# cocojumbo = []
+# bebra = ""
+# s = open("24-1.txt").readline()
+# for i in range(1, len(s)):
+#     if ord(s[i-1]) < ord(s[i]) :
+#         bebra += s[i-1]
+#     else:
+#         bebra += s[i-1]
+#         if bebra not in cocojumbo:
+#             cocojumbo.append(bebra)
+#         bebra = ""
+# print(max(cocojumbo, key=len))
+
+from itertools import product
+coun = 1
+maximka = 0
+cocojumbo = []
+
+'''cab = product('QRS', repeat=2)
+for m in cab:
+    k = ''.join(m)
+    cocojumbo.append(k)
+s = open("24_7600.txt").readline()
+for i in range(len(s)-1):
+    if (s[i] + s[i+1]) not in cocojumbo:
+        coun += 1
+    else:
+        maximka = max(maximka, coun)
+        coun = 1
+print(coun)
+'''
+
+# s = open("24_7600.txt").readline()
+# cab = product('QRS', repeat=2)
+# for m in cab:
+#     k = ''.join(m)
+#     s = s.replace(k, " ")
+# s = s.split()
+# print(len(max(s, key=len))+2)
